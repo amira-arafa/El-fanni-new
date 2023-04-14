af855c3c3985e17e955886ecb913f9c8db259f62
abdullahmuhammed5 <abdo56668@gmail.com>
2023-04-14 14:10:06 +0200

    refactor numbers in stats section

--- src/components/StatisticsCard/StatisticsCard.js
+++ src/components/StatisticsCard/StatisticsCard.js
@@ -3,25 +3,22 @@ import AnimatedNumber from "react-awesome-animated-number";
 import "react-awesome-animated-number/dist/index.css";
 import "./StatisticsCard.scss";
 
-const StatisticsCard = forwardRef(({ number, note, type, details }, ref) => {
+const StatisticsCard = forwardRef(({ prefix, value, postfix, note, type, details }, ref) => {
   return (
     <div className="statistics-card-container" ref={ref}>
       <div className="d-flex gap-2">
         <div className="percent">
-          {/* {percent} */}
+          <span>{prefix}</span>
           <AnimatedNumber
-            value={number}
+            value={value}
             hasComma={true}
             size={55}
             duration={2000}
           />
-          <span>%</span>
+          <span>{postfix}</span>
         </div>
-        <div
-          className={`d-flex flex-column ${
-            note ? "justify-content-center" : "justify-content-end"
-          }`}
-        >
+
+        <div className={`d-flex flex-column ${ note ? "justify-content-center" : "justify-content-end"}`}>
           {note && <div className="notes">{note}</div>}
           {type && <div className="type">{type}</div>}
         </div>
--- src/containers/HomePage/HomeStatisticsSection/HomeStatisticsSection.js
+++ src/containers/HomePage/HomeStatisticsSection/HomeStatisticsSection.js
@@ -3,12 +3,12 @@ import "./HomeStatisticsSection.scss";
 import StatisticsCard from "../../../components/StatisticsCard/StatisticsCard";
 
 const HomeStatisticsSection = () => {
-  const [number,  setNumber]  = useState(80);
-  const [number1, setNumber1] = useState(90);
-  const [number2, setNumber2] = useState(70);
-  const [number3, setNumber3] = useState(66);
-  const [number4, setNumber4] = useState(20);
-  const [number5, setNumber5] = useState(11);
+  const [number,  setNumber]  = useState(0);
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [number3, setNumber3] = useState(0);
+  const [number4, setNumber4] = useState(0);
+  const [number5, setNumber5] = useState(0);
   
   const myRef = useRef();
   
@@ -16,12 +16,12 @@ const HomeStatisticsSection = () => {
     const observer = new IntersectionObserver((entries, observer) => {
       const entry = entries[0];
       if (entry.intersectionRatio > 0) {
-        setNumber(number + 8);
-        setNumber1(number1 + 4);
-        setNumber2(number2 + 6);
-        setNumber3(number3 + 9);
-        setNumber4(number4 + 5);
-        setNumber5(number5 + 2);
+        setNumber(number + 1.4);
+        setNumber1(number1 + 60);
+        setNumber2(number2 + 9);
+        setNumber3(number3 + 28);
+        setNumber4(number4 + 36);
+        setNumber5(number5 + 28);
         setTimeout(() => {
           observer.unobserve(myRef.current);
         }, 5000);
@@ -36,26 +36,30 @@ const HomeStatisticsSection = () => {
         <div className="col-sm-4">
           <StatisticsCard
             ref={myRef}
-            number={number}
+            value={number}
             percent="+0.0"
             type="Billion"
+            prefix={"+"}
             details="Africa's Population"
           />
         </div>
         <div className="col-sm-4">
           <StatisticsCard
-            number={number1}
+            value={number1}
             percent="00"
+            prefix={"+"}
+            postfix={"%"}
             type="by 2030"
             details="Africa's Population under 25"
           />
         </div>
         <div className="col-sm-4">
           <StatisticsCard
-            number={number2}
+            value={number2}
             percent=""
             className="percent-3"
             type="Billion"
+            prefix={"$"}
             details="Allocated by EA to manage African irregular migration"
           />
         </div>
@@ -63,22 +67,24 @@ const HomeStatisticsSection = () => {
       <div className="d-flex gap-3 section-lower">
         <div className="col-sm-4">
           <StatisticsCard
-            number={number3}
+            value={number3}
             percent="00"
+            postfix={"%"}
             details="Africa's youth aren't in education, Employment, or training"
           />
         </div>
         <div className="col-sm-4">
           <StatisticsCard
             percent="00"
-            number={number4}
+            value={number4}
+            postfix={"%"}
             type="by 2024"
             details="Middle east's Population are expected to hit poverty"
           />
         </div>
         <div className="col-sm-4">
           <StatisticsCard
-            number={number5}
+            value={number5}
             percent="00"
             note={"(14% of Workforce)"}
             type="occupations"
